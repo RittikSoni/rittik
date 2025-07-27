6f80c625e493bf10c4d3981b71016d6791f043b2
RittikSoni <kingrittiksoni@gmail.com>
2025-07-27 09:29:44 +0530

    add speed inshights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import AmbientSound from "@/components/music/AmbientSound";
 
 export default function App({ Component, pageProps }) {
@@ -8,7 +8,9 @@ export default function App({ Component, pageProps }) {
     <>
       {" "}
       <AmbientSound />
-      <Component {...pageProps} /> <Analytics />
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
